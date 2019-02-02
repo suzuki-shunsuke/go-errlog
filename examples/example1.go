efe6f85756210468ee114185f2028a51a2090cde
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-02-02 10:38:28 +0900

    feat: improve Logger interface
    Close #8
    
    * add msgs fields to Logger
    * add Logger.With and Logger.Withf method
    * add Logger.xxxf methods (Debugf, Infof, Fatalf, Warnf, Errorf)
    * add fields and msgs arguments to Logger's logging method (Debug, Info, Fatal, Warn, Error)
    
    BREAKING CHANGE:
    
    * remove logger.WithFields and logger.WithField methods
    * fields argument is required for Logger's logging method (Debug, Info, Fatal, Warn, Error)

--- examples/example1.go
+++ examples/example1.go
@@ -10,19 +10,19 @@ import (
 
 func foo() (int, error) {
 	age, err := getAge("foo")
-	return age, errlog.Wrap(err, nil, "failed to foo")
+	return age, errlog.Wrap(err, logrus.Fields{
+		"name": "foo",
+	}, "failed to get age")
 }
 
 func getAge(name string) (int, error) {
-	return 0, errlog.New(logrus.Fields{
-		"name": name,
-	}, "invalid name", "failed to get an age")
+	return 0, fmt.Errorf("invalid name")
 }
 
 func main() {
 	logger := errlog.NewLogger(nil).
-		WithFields(logrus.Fields{"program": "example1"})
+		With(logrus.Fields{"program": "example1"}, "example is failure")
 	age, err := foo()
-	logger.Fatal(err) // you don't have to check err is nil or not.
+	logger.Fatal(err, nil, "function foo is failure") // you don't have to check err is nil or not.
 	fmt.Printf("age: %d\n", age)
 }
