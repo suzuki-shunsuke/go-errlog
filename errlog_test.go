474674871218b2a7f7a5574c75aaac5715414977
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-07-22 09:01:48 +0900

    test: add tests and use table driven test

--- errlog_test.go
+++ errlog_test.go
@@ -9,43 +9,172 @@ import (
 )
 
 func TestCheckField(t *testing.T) {
-	require.False(t, CheckField(nil, "foo", func(v interface{}) bool {
+	f := func(v interface{}) bool {
 		return v == 1
-	}))
+	}
 	var e *Error
-	require.False(t, CheckField(e, "foo", func(v interface{}) bool {
-		return v == 1
-	}))
-	require.False(t, CheckField(fmt.Errorf("foo"), "foo", func(v interface{}) bool {
-		return v == 1
-	}))
+	data := []struct {
+		title string
+		err   error
+		exp   bool
+	}{
+		{
+			title: "err is nil",
+		},
+		{
+			title: "Error is nil",
+			err:   e,
+		},
+		{
+			title: "not Error",
+			err:   fmt.Errorf("foo"),
+		},
+		{
+			title: "Error doesn't have the key",
+			err:   &Error{},
+		},
+		{
+			title: "the function returns false",
+			err:   &Error{fields: logrus.Fields{"foo": 0}},
+		},
+		{
+			title: "the function returns true",
+			err:   &Error{fields: logrus.Fields{"foo": 1}},
+			exp:   true,
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			if d.exp {
+				require.True(t, CheckField(d.err, "foo", f))
+				return
+			}
+			require.False(t, CheckField(d.err, "foo", f))
+		})
+	}
 }
 
 func TestGetField(t *testing.T) {
-	_, ok := GetField(nil, "foo")
-	require.False(t, ok)
-	_, ok = GetField(fmt.Errorf("foo"), "foo")
-	require.False(t, ok)
 	var e *Error
-	_, ok = GetField(e, "foo")
-	require.False(t, ok)
-	v, ok := GetField(&Error{fields: logrus.Fields{"foo": "bar"}}, "foo")
-	require.True(t, ok)
-	require.Equal(t, "bar", v)
+	data := []struct {
+		title string
+		err   error
+		expV  interface{}
+		expB  bool
+	}{
+		{
+			title: "err is nil",
+		},
+		{
+			title: "err is a normal error",
+			err:   fmt.Errorf("foo"),
+		},
+		{
+			title: "err is an Error but nil",
+			err:   e,
+		},
+		{
+			title: "err is an Error",
+			err:   &Error{fields: logrus.Fields{"foo": "bar"}},
+			expV:  "bar",
+			expB:  true,
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			v, ok := GetField(d.err, "foo")
+			if !d.expB {
+				require.False(t, ok)
+				return
+			}
+			require.True(t, ok)
+			require.Equal(t, d.expV, v)
+		})
+	}
 }
 
 func TestHasField(t *testing.T) {
-	require.False(t, HasField(nil, "foo"))
-	require.False(t, HasField(fmt.Errorf("foo"), "foo"))
 	var e *Error
-	require.False(t, HasField(e, "foo"))
+	data := []struct {
+		title string
+		err   error
+		exp   bool
+	}{
+		{
+			title: "err is nil",
+		},
+		{
+			title: "err is not Error",
+			err:   fmt.Errorf("foo"),
+		},
+		{
+			title: "err is an Error but nil",
+			err:   e,
+		},
+		{
+			title: "err is an Error",
+			err:   &Error{fields: logrus.Fields{"foo": "bar"}},
+			exp:   true,
+		},
+		{
+			title: "err is an Error but doesn't have the field",
+			err:   &Error{},
+			exp:   false,
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			if d.exp {
+				require.True(t, HasField(d.err, "foo"))
+				return
+			}
+			require.False(t, HasField(d.err, "foo"))
+		})
+	}
 }
 
 func TestHasMsg(t *testing.T) {
-	require.False(t, HasMsg(nil, "foo"))
-	require.True(t, HasMsg(fmt.Errorf("foo"), "foo"))
 	var e *Error
-	require.False(t, HasMsg(e, "foo"))
+	data := []struct {
+		title string
+		err   error
+		exp   bool
+	}{
+		{
+			title: "err is nil",
+		},
+		{
+			title: "err isn't an Error but has the message",
+			err:   fmt.Errorf("foo"),
+			exp:   true,
+		},
+		{
+			title: "err isn't Error",
+			err:   fmt.Errorf("bar"),
+		},
+		{
+			title: "err is an Error but nil",
+			err:   e,
+		},
+		{
+			title: "err is an Error but doesn't have the message",
+			err:   &Error{},
+		},
+		{
+			title: "err is an Error and has the message",
+			err:   &Error{msgs: []string{"foo"}},
+			exp:   true,
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			if d.exp {
+				require.True(t, HasMsg(d.err, "foo"))
+				return
+			}
+			require.False(t, HasMsg(d.err, "foo"))
+		})
+	}
 }
 
 func TestNew(t *testing.T) {
